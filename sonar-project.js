e2d4fff79532ccbd0990b3cb5b0af763f21d30d5
ramakrishnade <ramky.drk@gmail.com>
2024-02-25 23:56:53 +0530

    testing emails

---
+++ sonar-project.js
@@ -0,0 +1,15 @@
+const scanner = require('sonarqube-scanner');
+
+scanner(
+  {
+    serverUrl: 'http://192.168.0.11:9000/',
+    token: 'sqa_855e2848ad7fc2277d9b4e849b5bb8900246afed',
+    options: {
+      'sonar.projectName': 'hello-app',
+      'sonar.projectDescription': 'Description for "hello-app" project...',
+      'sonar.sources': 'src',
+      'sonar.tests': 'test',
+    },
+  },
+  () => process.exit(),
+);
